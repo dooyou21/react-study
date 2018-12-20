6393d554ec73defd09a22f8628563e97ac0df88c
dooyou21 <cubeclock94@gmail.com>
2018-12-20 09:22:19 +0900

    async dispatch_1

--- learncode-redux/src/App.js
+++ learncode-redux/src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
 import {applyMiddleware, createStore} from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import axios from 'axios';
+
 import reducers from './reducers';
 function App(props) {
   return (
     <h1>hello</h1>
   );
 }
-
-const logger = (store) => (next) => (action) => {
-  console.log('action fired', action);
-  next(action);
+/*
+const middle = (store) => (next) => (action) => {
+  // next(action);
 }
-
-const error = (store) => (next) => (action) => {
-  try {
-    next(action);
-  } catch (e) {
-    console.error("AHHHHH!!!", e);
-  }
-}
-
-const middleware = applyMiddleware(logger, error);
+*/
+const middleware = applyMiddleware(thunk, logger);
 
 const store = createStore(reducers, middleware);
 
@@ -31,6 +26,19 @@ store.subscribe(() => {
 store.dispatch({type: "CHANGE_NAME", payload: "Will"});
 store.dispatch({type: "CHANGE_AGE", payload: 26});
 store.dispatch({type: "TWEETS", payload: "asdf"});
-store.dispatch({type: "ERROR", payload: "asdf"});
+// store.dispatch({type: "ERROR", payload: "asdf"}); Actions must be plain
+// objects. Use custom middleware for async actions.
+store.dispatch((dispatch) => {
+  dispatch({type: "FETCH_USERS_START"});
+  axios
+    .get("http://rest.learncode.academy/api/wstern/users")
+    .then((response) => {
+      dispatch({type: "RECEIVE_USERS", payload: response.data});
+    })
+    .catch((err) => {
+      dispatch({type: "FETCH_USERS_ERROR", payload: err});
+    });
+  //do something async
+});
 
 export default App;
--- learncode-redux/src/reducers/userReducer.js
+++ learncode-redux/src/reducers/userReducer.js
@@ -1,5 +1,30 @@
-const userReducer = (state = {}, action) => {
+const initialState = {
+  fetching: false,
+  fetched: false,
+  users: [],
+  error: null
+}
+
+const userReducer = (state = initialState, action) => {
   switch (action.type) {
+    case "FETCH_USERS_START":
+      return {
+        ...state,
+        fetching: true
+      }
+    case "FETCH_USERS_ERROR":
+      return {
+        ...state,
+        fetching: false,
+        error: action.payload
+      }
+    case "RECEIVE_USERS":
+      return {
+        ...state,
+        fetching: false,
+        fetched: true,
+        users: action.payload
+      }
     case "CHANGE_NAME":
       state = {
         ...state,
