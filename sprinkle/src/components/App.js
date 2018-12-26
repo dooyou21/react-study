e36adb35483f36ccdd8f66ec54901beb4446d536
dooyou21 <cubeclock94@gmail.com>
2018-12-26 18:19:13 +0900

    make html

--- sprinkle/src/components/App.js
+++ sprinkle/src/components/App.js
@@ -2,7 +2,23 @@ import React from "react";
 const App = () => {
   return (
     <div>
-      <p>React Here!</p>
+      <header>
+        <div className="header">
+          <h1>Unsplash</h1>
+          <input
+            type="text"
+            maxLength="20"
+            placeholder="Search Free high-resolution photos"/>
+        </div>
+      </header>
+      <div className="container">
+        <div className="container__tile"></div>
+        <div className="container__tile"></div>
+        <div className="container__tile"></div>
+        <div className="container__tile"></div>
+        <div className="container__tile"></div>
+        <div className="container__tile"></div>
+      </div>
     </div>
   );
 };
